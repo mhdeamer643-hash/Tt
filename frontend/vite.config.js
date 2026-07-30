2fee616fdb607dffbce319afa6533f8c16f7cc89
mhdeamer643-hash <mhdeamer643@gmail.com>
2026-07-30 04:34:02 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
+  build: {
+    rollupOptions: {
+      external: ['اسم_الوحدة_التي_تريد_استبعادها']
+    }
+  }
 });
